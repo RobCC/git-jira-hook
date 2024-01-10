af08b32d11a7d1102258a32af56767f45e08f600
RobCC <rrc0138@gmail.com>
2024-01-10 13:59:59 -0600

    chore: list external deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,27 @@ import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 // import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-// import { preserveShebangs } from 'rollup-plugin-preserve-shebangs';
 
 export default {
   input: 'bin/commit-msg.ts',
+  external: [
+    'yargs',
+    'yargs/helpers',
+    'fs',
+    'ajv',
+    'url',
+    'path',
+    'app-root-path',
+    'current-git-branch',
+    'lodash.merge',
+  ],
   output: {
     banner: '#!/usr/bin/env node',
     file: 'build/commit-msg.cjs',
     format: 'cjs',
   },
-  // preserveModules: true,
   plugins: [
     typescript(),
-    // preserveShebangs(),
     // commonjs({
     //   dynamicRequireTargets: ["./bin/utils/config/reader.js"],
     // }),
